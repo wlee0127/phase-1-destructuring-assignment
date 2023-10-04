60af4003b84747c4a04df17e2eb73cd6796fc9e4
william lee <will.lee.npc@gmail.com>
2023-10-03 20:48:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, ,black, pink,] = farmAnimals.split(' ');
+
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red,orange,yellow,green,blue,indigo,violet] = colors;
+const [r,o,y,g,b,,v] = colors;
+const [,,,,,indg,] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +17,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const {muppetName,color,song,job,partner}=muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +34,9 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const {nestedJob,nestedPartner} = nestedMuppet;
+const {song2,song4} = nestedMuppet.album.theMuppetMovie;
 
 // Strings
 
